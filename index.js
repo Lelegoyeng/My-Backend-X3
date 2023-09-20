38a35c793b31120f68f2944ec51880fc0e0a1f77
lelegoyeng <lelegoyengs@gmail.com>
2023-09-20 10:14:37 +0700

    update

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ const api = new MetaApi(token);
 
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 function authorizeToken(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
-    // console.log(token)
-    if (!token) {
+    if (!req.headers.authorization) {
         return res.status(403).json({ message: 'Tidak ada token tersedia' });
     }
+
+    const token = req.headers.authorization.split(' ')[1];
 
     jwt.verify(token, secretKey, (err, user) => {
         if (err) {
@@ -61,11 +60,6 @@ app.get('/', authorizeToken, async (req, res) => {
         currency: terminalState.accountInformation.currency
     }
 
-    // const tanggal = new Date().toISOString().split('T')[0];
-    // const startDate = new Date(`${tanggal}T00:00:00.000Z`);
-    // const endDate = new Date(`${tanggal}T23:59:59.000Z`);
-    // console.log(startDate, 'startdate && ', endDate, 'enbdate');
-    // const HistoryOrder = connection.historyStorage.getHistoryOrdersByTimeRange(new Date(Date.now() - 24 * 60 * 60 * 1000), new Date());
     const HistoryDeal = connection.historyStorage.getDealsByTimeRange(new Date(Date.now() - 24 * 60 * 60 * 1000), new Date());
     const ProfitDay = HistoryDeal?.reduce((a, b) => a + b.profit, 0);
     const Position = terminalState.positions
@@ -75,9 +69,28 @@ app.get('/', authorizeToken, async (req, res) => {
         ProfitDay: ProfitDay,
         Position: Position
     }
-    res.json({ result: result });
+    res.status(200).json({ result: result });
 });
 
+
+app.post('/closeposition', authorizeToken, async (req, res) => {
+    const account = await api.metatraderAccountApi.getAccount(accountId);
+    const connection = account.getStreamingConnection();
+    await connection.connect();
+    const terminalState = connection.terminalState;
+    await connection.waitSynchronized();
+    const position = terminalState.positions?.map(get => { return get.id })
+
+    const { position_id } = req.body;
+    if (!position_id) return res.status(403).json({ message: 'ID Position Tidak Ada!' });
+    if (!position.includes(position_id)) {
+        return res.status(401).json({ message: 'Gagal Close Position ID Tidak Valid!' });
+
+    } else {
+        await connection.closePosition(position_id)
+        return res.status(201).json({ message: `Close Position Success ${position_id}` });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
